540944e17dca26af3a4081427d911e4ff1007951
Anshu Sinha <anshujuly2@gmail.com>
2023-03-03 11:11:24 +0530

    Add day16 contructing obejcts codes

---
+++ main.py
@@ -0,0 +1,14 @@
+## 147. Constructing objects and accessing their attributs and methods
+
+# import turtle 
+# from turtle import Turtle, Screen
+
+# timmy = Turtle()
+# myScreen = Screen()
+
+# timmy.shape("turtle")
+# timmy.color("blue")
+# timmy.fd(100)
+
+# print(myScreen.canvheight)
+# myScreen.exitonclick()
