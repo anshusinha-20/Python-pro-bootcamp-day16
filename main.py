afbc1e5c17bd3b40a3d998c3de3cd8fd386b6002
Anshu Sinha <anshujuly2@gmail.com>
2023-03-03 20:31:00 +0530

    Add modifying object attributes and calling methods codes

--- main.py
+++ main.py
@@ -12,3 +12,24 @@
 
 # print(myScreen.canvheight)
 # myScreen.exitonclick()
+
+#####
+
+## 149. Practice modifying object attributes and calling methods
+
+# """from prettytable module PrettyTable class is imported"""
+
+# from prettytable import PrettyTable
+
+# """table object is created"""
+# table = PrettyTable()
+# """pokemons list is created"""
+# pokemons = ["Bulbasaur", "Ivysaur", "Charmander"]
+# """pokemonTypes list is created"""
+# pokemonTypes = ["Grass", "Grass", "Fire"]
+# """a column is added to the table"""
+# table.add_column("Pokemon Name", pokemons)
+# """another columns is added to the table"""
+# table.add_column("Types", pokemonTypes)
+# """the table object is printed"""
+# print(table)
